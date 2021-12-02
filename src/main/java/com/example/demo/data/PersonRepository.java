f6e04b21d13b46ecf442c452f490ab58aed5a810
Wolmir Cezer Garbin <wolmir.garbin@mercadolivre.com>
2021-12-01 22:57:33 -0300

    Criado consulta de pessoa por nome

--- src/main/java/com/example/demo/data/PersonRepository.java
+++ src/main/java/com/example/demo/data/PersonRepository.java
@@ -3,5 +3,9 @@ package com.example.demo.data;
 import com.example.demo.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    List<Person> findAllByNameContains(String search);
 }
--- src/test/java/com/example/demo/data/PersonRepositoryTest.java
+++ src/test/java/com/example/demo/data/PersonRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.demo.data;
 
 import com.example.demo.model.Person;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,11 @@ public class PersonRepositoryTest {
 
     @Autowired
     private PersonRepository personRepository;
+
+    @BeforeEach
+    public void init() {
+        personRepository.deleteAll();
+    }
 
     @Test
     public void testSaveAndCount() {
@@ -39,5 +45,26 @@ public class PersonRepositoryTest {
         Assertions.assertEquals("Test", list.get(0).getName());
         Assertions.assertEquals("Persistence", list.get(0).getLastName());
         Assertions.assertEquals(71, list.get(0).getYears());
+    }
+
+    @Test
+    public void testQueryByName() {
+        Person person = new Person();
+        person.setName("Willian");
+        person.setLastName("Persistence");
+        person.setYears(21);
+
+        personRepository.save(person);
+
+        person = new Person();
+        person.setName("Cesar");
+        person.setLastName("Persistence");
+        person.setYears(31);
+
+        personRepository.save(person);
+
+        var list = personRepository.findAllByNameContains("Wil");
+
+        Assertions.assertEquals(1, list.size());
     }
 }
