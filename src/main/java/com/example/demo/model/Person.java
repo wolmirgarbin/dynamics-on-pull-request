75846e12290d8d4395bff92febc279a021a997ff
Wolmir Cezer Garbin <wolmir.garbin@mercadolivre.com>
2021-12-01 22:11:46 -0300

    Estrutura principal

---
+++ src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(name = "last_name")
+    private String lastName;
+    private Integer years;
+
+}
